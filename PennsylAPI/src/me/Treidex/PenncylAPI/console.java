bc721c131477231cca5bdb8335cf9b82e1c8d98f
Treidex <telkids4@gmail.com>
2019-12-04 20:56:10 -0800

    Add files via upload

---
+++ PennsylAPI/src/me/Treidex/PenncylAPI/console.java
@@ -0,0 +1,16 @@
+package me.Treidex.PenncylAPI;
+
+public class console {
+	
+	console (String out) {
+		
+	}
+
+	public static void log(String s) {
+		System.out.println(s);
+	}
+	
+	public static void error(String s) {
+		System.err.println(s);
+	}
+}
